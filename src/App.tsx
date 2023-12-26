b14a2995d228d72a4fdaa4d6ac2e1fdfd7f126cf
Shivam-Panda <panda.shivam81@bloomfield.org>
2023-12-26 13:24:43 -0500

    Basic Game Play

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const MainBoard = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8,
+]
+
 function App() {
+  const [vals, setVals] = useState<String[]>([]);
+  const [turn, setTurn] = useState<number>(0);
+  const [active, setActive] = useState<number>(4);
+
+  const handleClick = (outer_index: number, inner_index: number) => {
+    if(outer_index === active) {
+      const sum = (outer_index * 100) + inner_index
+      if(turn % 2 === 0) {
+        vals[sum] = "X"
+      } else {
+        vals[sum] = "O"
+      }
+      setActive(inner_index)
+      setTurn(turn + 1);
+    } else {
+      alert("Invalid Turn")
+    }
+  }
+
   return (
-    <div className="App">
-      Tic-Tac-Toe
+    <div className="GRID">
+      {
+        MainBoard.map((val, index) => (
+              <div>
+                <div className="gridItem">
+                {
+                  MainBoard.map((_v, _i) => (
+                    <div>
+                      <div onClick={() => {handleClick(index, _i)}} className="innerItem">
+                        <span className="text">{vals[(index * 100) + _i]}</span>
+                      </div>
+                    </div>
+                  ))
+                }
+
+                </div>
+              </div>
+        ))
+      }
     </div>
   );
 }
